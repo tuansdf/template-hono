103f71bde68e69daf945a7b9a26dc419527bf88a
tuansdf <tuanna191101@gmail.com>
2024-05-04 02:07:29 +0700

    docker

--- src/middlewares/not-found.middleware.ts
+++ src/middlewares/not-found.middleware.ts
@@ -3,6 +3,6 @@ import { NotFoundHandler } from "hono";
 export const notFound = (): NotFoundHandler => {
   return (c) => {
     const t = c.get("t");
-    return c.json({ status: 404, message: t("common.error.not_found") }, 400);
+    return c.json({ status: 404, message: t("common.error.not_found") }, 404);
   };
 };
