f360633d91746983e9811426589e304547e90f49
tuansdf <tuanna191101@gmail.com>
2024-05-04 18:10:14 +0700

    update

--- src/exceptions/custom-exception.ts
+++ src/exceptions/custom-exception.ts
@@ -3,6 +3,14 @@ import { StatusCode } from "~/types/status-code.type.js";
 
 export class CustomException extends HTTPException {
   constructor(message?: string, status?: StatusCode) {
-    super(status || 400, { message: message || "Bad Request" });
+    const s = status || 400;
+    const m = message || "Bad Request";
+    super(s, { message: m });
+
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, this.constructor);
+    } else {
+      this.stack = new HTTPException(s, { message: m }).stack;
+    }
   }
 }
