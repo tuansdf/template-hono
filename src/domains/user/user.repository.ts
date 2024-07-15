4bcc521a34d02c79c93a63d813618fe48c5c2ab6
tuansdf <103062682+tuansdf@users.noreply.github.com>
2024-07-15 21:21:48 +0700

    update

--- src/domains/user/user.repository.ts
+++ src/domains/user/user.repository.ts
@@ -1,6 +1,6 @@
 import { count, eq, or } from "drizzle-orm";
 import { db } from "~/database/db";
-import { UserSave } from "~/domains/user/user.type";
+import { UserDTO, UserSave } from "~/domains/user/user.type";
 import { UserTable } from "~/entities/user.entity";
 
 export const userSelector = {
@@ -26,91 +26,91 @@ export const userSelector = {
 } as const;
 
 class UserRepository {
-  public async findAll() {
+  public async findAll(): Promise<UserDTO[]> {
     return db.main.select(userSelector.all).from(UserTable);
   }
 
-  public async findAllByStatus(status: string) {
+  public async findAllByStatus(status: string): Promise<UserDTO[]> {
     return db.main.select(userSelector.all).from(UserTable).where(eq(UserTable.status, status));
   }
 
-  public async findTopById(id: number) {
+  public async findTopById(id: number): Promise<UserDTO | undefined> {
     const result = await db.main.select(userSelector.all).from(UserTable).where(eq(UserTable.id, id)).limit(1);
-    return result?.[0];
+    return result[0];
   }
 
-  public async findTopByUsernameOrEmailWithPassword(username: string) {
+  public async findTopByUsernameOrEmailWithPassword(username: string): Promise<UserDTO | undefined> {
     const result = await db.main
       .select(userSelector.all)
       .from(UserTable)
       .where(or(eq(UserTable.username, username), eq(UserTable.email, username)))
       .limit(1);
-    return result?.[0];
+    return result[0];
   }
 
-  public async findTopByUsernameOrEmail(username: string) {
+  public async findTopByUsernameOrEmail(username: string): Promise<UserDTO | undefined> {
     const result = await db.main
       .select(userSelector.all)
       .from(UserTable)
       .where(or(eq(UserTable.username, username), eq(UserTable.email, username)))
       .limit(1);
-    return result?.[0];
+    return result[0];
   }
 
-  public async findTopByUsername(username: string) {
+  public async findTopByUsername(username: string): Promise<UserDTO | undefined> {
     const result = await db.main
       .select(userSelector.all)
       .from(UserTable)
       .where(eq(UserTable.username, username))
       .limit(1);
-    return result?.[0];
+    return result[0];
   }
 
-  public async findTopByEmail(email: string) {
+  public async findTopByEmail(email: string): Promise<UserDTO | undefined> {
     const result = await db.main.select(userSelector.all).from(UserTable).where(eq(UserTable.email, email)).limit(1);
-    return result?.[0];
+    return result[0];
   }
 
-  public async countByUsername(username: string) {
+  public async countByUsername(username: string): Promise<number> {
     const result = await db.main.select({ value: count() }).from(UserTable).where(eq(UserTable.username, username));
-    return result?.[0]?.value || 0;
+    return result[0]?.value || 0;
   }
 
-  public async countByUsernameOrEmail(username: string) {
+  public async countByUsernameOrEmail(username: string): Promise<number> {
     const result = await db.main
       .select({ value: count() })
       .from(UserTable)
       .where(or(eq(UserTable.username, username), eq(UserTable.email, username)));
-    return result?.[0]?.value || 0;
+    return result[0]?.value || 0;
   }
 
-  public async existByUsernameOrEmail(username: string) {
+  public async existByUsernameOrEmail(username: string): Promise<boolean> {
     const result = await this.countByUsernameOrEmail(username);
     return result > 0;
   }
 
-  public async existByUsername(username: string) {
+  public async existByUsername(username: string): Promise<boolean> {
     const result = await this.countByUsername(username);
     return result > 0;
   }
 
-  public async countByEmail(email: string) {
+  public async countByEmail(email: string): Promise<number> {
     const result = await db.main.select({ value: count() }).from(UserTable).where(eq(UserTable.email, email));
-    return result?.[0]?.value || 0;
+    return result[0]?.value || 0;
   }
 
-  public async existByEmail(email: string) {
+  public async existByEmail(email: string): Promise<boolean> {
     const result = await this.countByEmail(email);
     return result > 0;
   }
 
-  public async save(user: UserSave) {
+  public async save(user: UserSave): Promise<UserDTO | undefined> {
     const result = await db.main.insert(UserTable).values(user).returning(userSelector.all);
-    return result[0]!;
+    return result[0];
   }
 
-  public async saveAll(users: UserSave[]) {
-    return db.main.insert(UserTable).values(users);
+  public async saveAll(users: UserSave[]): Promise<void> {
+    await db.main.insert(UserTable).values(users);
   }
 }
 
