613389a659deff8c3c529ab4dd4a7f79e8436e58
tuansdf <103062682+tuansdf@users.noreply.github.com>
2024-07-15 16:44:26 +0700

    migrate

--- src/migrate.ts
+++ src/migrate.ts
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import { drizzle } from "drizzle-orm/node-postgres";
 import { migrate } from "drizzle-orm/node-postgres/migrator";
+import * as process from "node:process";
 import { Client } from "pg";
 import { ENV_DB_DATABASE_NAME, ENV_DB_HOST, ENV_DB_PASSWORD, ENV_DB_PORT, ENV_DB_USER } from "~/constants/env.constant";
 
@@ -14,3 +15,5 @@ const client = new Client({
 const db = drizzle(client);
 await migrate(db, { migrationsFolder: "./drizzle" });
 await client.end();
+console.info("DONE");
+process.exit(0);
