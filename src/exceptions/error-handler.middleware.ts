2281962d93ab9083625dd50de58e240817bf2dad
tuansdf <103062682+tuansdf@users.noreply.github.com>
2024-07-15 15:46:01 +0700

    update

--- src/exceptions/error-handler.middleware.ts
+++ src/exceptions/error-handler.middleware.ts
@@ -1,6 +1,13 @@
 import { ErrorHandler } from "hono";
 import { exceptionUtils } from "~/exceptions/exception.util";
+import { logger } from "~/lib/logger/logger";
+import { routerUtils } from "~/utils/router.util";
 
 export const errorHandler = (): ErrorHandler => {
-  return exceptionUtils.response;
+  return (err, c) => {
+    const t = c.get("t");
+    logger.error(err);
+    const commonResponse = exceptionUtils.getCommonResponse(err, t, true);
+    return routerUtils.response(c, commonResponse.status, { message: commonResponse.message });
+  };
 };
--- src/exceptions/exception.util.ts
+++ src/exceptions/exception.util.ts
@@ -1,48 +1,33 @@
-import { ErrorHandler } from "hono";
 import { HTTPException } from "hono/http-exception";
 import { ZodError } from "zod";
 import { CustomException } from "~/exceptions/custom-exception";
 import { TFn } from "~/i18n/i18n.type";
 import { i18nUtils } from "~/i18n/i18n.util";
-import { logger } from "~/lib/logger/logger";
-import { routerUtils } from "~/utils/router.util";
+import { CommonResponse } from "~/types/common.type";
+import { StatusCode } from "~/types/status-code.type";
 
 class ExceptionUtils {
-  public response: ErrorHandler = (err, c) => {
-    const t = c.get("t");
-    const errorPrefix = t("field.error_c") + ": ";
-    logger.error(err);
+  public getCommonResponse = (err: Error, t: TFn, prefix: boolean = true): CommonResponse => {
+    let errorPrefix = prefix ? t("field.error_c") + ": " : "";
+    let errorMessage = "";
+    let status: StatusCode = 500;
     if (err instanceof CustomException) {
-      return routerUtils.response(c, err.status || 400, {
-        message: errorPrefix + i18nUtils.getMessageAndParams(t, err.message || "generic.error.other"),
-      });
+      status = err.status || 400;
+      errorMessage = i18nUtils.getMessageAndParams(t, err.message || "generic.error.other");
     }
     if (err instanceof HTTPException) {
-      return routerUtils.response(c, err.status || 500, {
-        message: errorPrefix + i18nUtils.getMessageAndParams(t, err.message || "generic.error.other"),
-      });
+      status = err.status || 500;
+      errorMessage = i18nUtils.getMessageAndParams(t, err.message || "generic.error.other");
     }
     if (err instanceof ZodError) {
-      return routerUtils.response(c, 400, {
-        message: errorPrefix + i18nUtils.getMessageAndParams(t, err.errors[0]?.message || "generic.error.other"),
-      });
+      status = 400;
+      errorMessage = i18nUtils.getMessageAndParams(t, err.errors[0]?.message || "generic.error.other");
     }
-    return routerUtils.response(c, 500, { message: errorPrefix + t("generic.error.other") });
+    return { message: errorPrefix + (errorMessage || t("generic.error.other")), status };
   };
 
   public getMessage = (err: Error, t: TFn, prefix: boolean = true): string => {
-    let errorMessage = "";
-    let errorPrefix = prefix ? t("field.error_c") + ": " : "";
-    if (err instanceof CustomException) {
-      errorMessage = i18nUtils.getMessageAndParams(t, err.message || "generic.error.other");
-    }
-    if (err instanceof HTTPException) {
-      errorMessage = i18nUtils.getMessageAndParams(t, err.message || "generic.error.other");
-    }
-    if (err instanceof ZodError) {
-      errorMessage = i18nUtils.getMessageAndParams(t, err.errors[0]?.message || "generic.error.other");
-    }
-    return errorPrefix + (errorMessage || t("generic.error.other"));
+    return this.getCommonResponse(err, t, prefix).message!;
   };
 }
 
