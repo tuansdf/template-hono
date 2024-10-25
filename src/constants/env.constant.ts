2471949d64e022ecc11dca067ce16a6aebafd66f
tuanna96 <tuanna96@vpbank.com.vn>
2024-10-25 15:25:31 +0700

    update router

--- src/constants/env.constant.ts
+++ src/constants/env.constant.ts
@@ -2,16 +2,16 @@ import * as process from "node:process";
 import { z } from "zod";
 
 const schema = z.object({
-  PORT: z.coerce.number(),
-  DB_URL: z.coerce.string(),
-  JWT_SECRET: z.coerce.string(),
-  JWT_ACCESS_LIFETIME: z.coerce.number(),
-  JWT_REFRESH_LIFETIME: z.coerce.number(),
-  TOKEN_RESET_PASSWORD_LIFETIME: z.coerce.number(),
-  TOKEN_ACTIVATE_ACCOUNT_LIFETIME: z.coerce.number(),
-  EMAIL_ACTIVATE_ACCOUNT_BASE_URL: z.coerce.string(),
-  EMAIL_RESET_PASSWORD_BASE_URL: z.coerce.string(),
-  ENV: z.coerce.string(),
+  PORT: z.coerce.number().min(1),
+  DB_URL: z.coerce.string().min(1),
+  JWT_SECRET: z.coerce.string().min(1),
+  JWT_ACCESS_LIFETIME: z.coerce.number().min(1),
+  JWT_REFRESH_LIFETIME: z.coerce.number().min(1),
+  TOKEN_RESET_PASSWORD_LIFETIME: z.coerce.number().min(1),
+  TOKEN_ACTIVATE_ACCOUNT_LIFETIME: z.coerce.number().min(1),
+  EMAIL_ACTIVATE_ACCOUNT_BASE_URL: z.coerce.string().min(1),
+  EMAIL_RESET_PASSWORD_BASE_URL: z.coerce.string().min(1),
+  ENV: z.coerce.string().min(1),
 });
 
 export const ENV = schema.parse(process.env);
--- src/i18n/i18n.constant.ts
+++ src/i18n/i18n.constant.ts
@@ -1,5 +1,5 @@
 import { ValidLang } from "~/i18n/i18n.type";
 
-export const validLangs: ValidLang[] = ["en", "vi"] as const;
+export const validLangs: ValidLang[] = ["en", "vi"];
 
 export const defaultLang: ValidLang = "en";
