2d7022940a57bf079f2cbc33e97e13f105189b46
tuanna96 <tuanna96@vpbank.com.vn>
2024-10-25 15:33:33 +0700

    update

--- src/domains/auth/auth.schema.ts
+++ src/domains/auth/auth.schema.ts
@@ -5,29 +5,7 @@ import {
   RegisterRequestDTO,
   ResetPasswordRequestDTO,
 } from "~/domains/auth/auth.type";
-
-const emailSchema = z
-  .string({
-    required_error: "form.error.missing:::field.email",
-    invalid_type_error: "form.error.invalid:::field.email",
-  })
-  .trim()
-  .email("form.error.invalid:::field.email");
-const usernameSchema = z
-  .string({
-    required_error: "form.error.missing:::field.username",
-    invalid_type_error: "form.error.invalid:::field.username",
-  })
-  .trim()
-  .min(2, "form.error.under_min_length:::field.username:::2")
-  .max(255, "form.error.over_max_length:::field.username:::255");
-const passwordSchema = z
-  .string({
-    required_error: "form.error.missing:::field.password",
-    invalid_type_error: "form.error.invalid:::field.password",
-  })
-  .min(8, "form.error.under_min_length:::field.password:::8")
-  .max(64, "form.error.over_max_length:::field.password:::64");
+import { emailSchema, passwordSchema, tokenSchema, usernameSchema } from "~/schemas/common.schema";
 
 export const loginRequestSchema: z.ZodType<LoginRequestDTO> = z.object({
   username: usernameSchema,
@@ -43,13 +21,6 @@ export const registerRequestSchema: z.ZodType<RegisterRequestDTO> = z.object({
 export const forgotPasswordRequestSchema: z.ZodType<ForgotPasswordRequestDTO> = z.object({
   username: usernameSchema,
 });
-
-const tokenSchema = z
-  .string({
-    required_error: "form.error.invalid:::field.token",
-    invalid_type_error: "form.error.invalid:::field.token",
-  })
-  .min(1, "form.error.invalid:::field.token");
 
 export const resetPasswordRequestSchema: z.ZodType<ResetPasswordRequestDTO> = z
   .object({
